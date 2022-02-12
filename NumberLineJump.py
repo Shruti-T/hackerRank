68f0ae32578f9dca2bf6f247d36bb0bda2f7fc8b
Shruti Tiwari <sh03shruti@gmail.com>
2022-02-12 19:40:31 +0530

    noLineJump

---
+++ NumberLineJump.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+#
+# Complete the 'kangaroo' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. INTEGER x1
+#  2. INTEGER v1
+#  3. INTEGER x2
+#  4. INTEGER v2
+#
+
+def kangaroo(x1, v1, x2, v2):
+    # Write your code here
+    d1=x1
+    d2=x2
+    for i in range(0,10001):
+        d1+=v1
+        d2+=v2
+        if(d1==d2):
+            a=1
+            break
+        else:
+            a=0
+    if(a==1):
+        return "YES"
+    else:
+        return "NO"
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    x1 = int(first_multiple_input[0])
+
+    v1 = int(first_multiple_input[1])
+
+    x2 = int(first_multiple_input[2])
+
+    v2 = int(first_multiple_input[3])
+
+    result = kangaroo(x1, v1, x2, v2)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
