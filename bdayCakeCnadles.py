551b41a8cbe941f21d8016218ea041987a91e49c
Shruti Tiwari <sh03shruti@gmail.com>
2022-02-13 22:30:42 +0530

    question pt2

--- bdayCakeCnadles.py
+++ bdayCakeCnadles.py
@@ -1,11 +1,17 @@
-#!/bin/python3
+# You are in charge of the cake for a child's birthday. You have decided the cake will have one candle for each year of their total age. They will only be able to blow out the tallest of the candles. Count how many candles are tallest.
 
-import math
-import os
-import random
-import re
-import sys
+# Example
+# candels=[4,4,1,3]
 
+# The maximum height candles are 4 units high. There are 2 of them, so return 2.
+
+# Function Description
+
+# Complete the function birthdayCakeCandles in the editor below.
+# birthdayCakeCandles has the following parameter(s):
+# int candles[n]: the candle heights
+
+# ---------SOLTUION-------------
 #
 # Complete the 'birthdayCakeCandles' function below.
 #
