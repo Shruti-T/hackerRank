77094509feb769851ad0894ed9cdb55ad61daf26
Shruti Tiwari <sh03shruti@gmail.com>
2022-02-15 21:30:10 +0530

    question3dSurface

--- medium/3dSurfaceArea.py
+++ medium/3dSurfaceArea.py
@@ -1,3 +1,12 @@
+# Madison is a little girl who is fond of toys. Her friend Mason works in a toy manufacturing factory . Mason has a 2D board A  of size H x W  with H rows and W columns. The board is divided into cells of size 1x1 with each cell indicated by its coordinate (i,j) . The cell (i,j) has an integer Aij written on it. To create the toy Mason stacks Aij number of cubes of size 1x1x1 on the cell (i,j).
+
+# Given the description of the board showing the values of Aij and that the price of the toy is equal to the 3d surface area find the price of the toy.
+
+# Input Format
+# The first line contains two space-separated integers H and W the height and the width of the board respectively.
+# The next H lines contains W space separated integers. The jth integer in ith line denotes Aij.
+
+
 # ----------------------SOLUTION------------------------------------
 #
 # Complete the 'surfaceArea' function below.
