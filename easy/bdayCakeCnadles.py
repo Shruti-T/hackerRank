0fc55115c5b76919c9b98eb0ed363ac63b95b043
Shruti Tiwari <sh03shruti@gmail.com>
2022-03-26 23:14:19 +0530

    -

--- easy/bdayCakeCnadles.py
+++ easy/bdayCakeCnadles.py
@@ -18,6 +18,14 @@
 # The function is expected to return an INTEGER.
 # The function accepts INTEGER_ARRAY candles as parameter.
 #
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
 
 def birthdayCakeCandles(candles):
     # Write your code here
