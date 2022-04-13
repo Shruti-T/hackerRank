f66e50498559a01b380f789654af5615dd20f3f7
Shruti Tiwari <sh03shruti@gmail.com>
2022-04-13 21:07:21 +0530

    timeConvension

---
+++ easy/timeConvension.py
@@ -0,0 +1,37 @@
+# Given a time in 12-hour AM/PM format, convert it to military (24-hour) time.
+
+# ---------------SOLUTION--------------------
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+def timeConversion(s):
+    # Write your code here
+    first=s[0:2]
+    time=s[-2:]
+    if time=="PM":
+        if(first!="12"):
+            newTime=str(int(first)+12)
+        else:
+            newTime="12"
+    else:
+        if(first=="12"):
+            newTime="00"
+        else:
+            newTime=first
+    
+    final=newTime+s[2:8]
+    return final
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = timeConversion(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
