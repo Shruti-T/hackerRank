fdba723092c8987c299bacdb54fdceba60183287
Shruti Tiwari <sh03shruti@gmail.com>
2022-07-06 22:44:48 +0530

    designerPdf

---
+++ easy/Designer pdf .py
@@ -0,0 +1,47 @@
+# There is a list of 26 character heights aligned by index to their letters. For example, 'a' is at index 0 and 'z' is at index 25. There will also be a string. Using the letter heights given, determine the area of the rectangle highlight in (mm)^2 assuming all letters are 1mm wide.
+            
+
+# -----------------------------------------------------SOLUTION----------------------------------------------
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'designerPdfViewer' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY h
+#  2. STRING word
+#
+
+def designerPdfViewer(h, word):
+    # Write your code here
+    arr=[]
+    temp=sorted(word)
+    
+    for i in temp:
+        index=ord(i)-97
+        arr.append(h[index])
+        
+    area=(len(word))*(max(arr))
+    return area
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    h = list(map(int, input().rstrip().split()))
+
+    word = input()
+
+    result = designerPdfViewer(h, word)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
