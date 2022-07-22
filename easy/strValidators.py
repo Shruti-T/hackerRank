c9a39ed5b3fe26b6f8bc64ee99e03ac79dd03725
Shruti Tiwari <sh03shruti@gmail.com>
2022-07-23 00:01:44 +0530

    strValidators

---
+++ easy/strValidators.py
@@ -0,0 +1,12 @@
+# Python has built-in string validation methods for basic data. It can check if a string is composed of alphabetical characters, alphanumeric characters, digits, etc.
+
+# SOME IMPORTANT INBUILT METHODS---
+
+# ---------------------------------SOLUTION--------------------
+if __name__ == '__main__':
+    s = raw_input()
+    print any(c.isalnum() for c in s)
+    print any(c.isalpha() for c in s)
+    print any(c.isdigit() for c in s)
+    print any(c.islower() for c in s)
+    print any(c.isupper() for c in s)
