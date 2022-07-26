b2c3b3122dd0f6dff81fdcac7a73dff1e76668f3
Shruti Tiwari <sh03shruti@gmail.com>
2022-07-26 07:36:51 +0530

    new

---
+++ medium/noIdea.py
@@ -0,0 +1,2 @@
+# There is an array of n integers. There are also 2 disjoint sets, A and B, each containing  integers. You like all the integers in set A and dislike all the integers in set B. Your initial happiness is 0. For each i integer in the array, if i bllongs to A, you add 1 to your happiness. If i belongs to B, you add -1 to your happiness. Otherwise, your happiness does not change. Output your final happiness at the end.
+
