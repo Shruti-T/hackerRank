8e1ab2ee35223f4ed6ded4a4928c1df144313e89
Shruti Tiwari <sh03shruti@gmail.com>
2022-08-25 07:48:40 +0530

    question

--- easy/Sequence Equation.py
+++ easy/Sequence Equation.py
@@ -1,6 +1,6 @@
-# Given a sequence of  integers,  where each element is distinct and satisfies . For each  where , 
-# that is  increments from  to , find any integer  
-# such that  and keep a history of the values of  in a return array.
+# Given a sequence of n integers, p(1), p(2),...,p(n) where each element is distinct and satisfies 1 <=p(x) <=n. 
+# For each x where 1 <= x <=n, that is x increments from 1 to n, find any integer y such that p(p(y)) = x and 
+# keep a history of the values of y in a return array.
 
 # ---------------------------SOLUTION-----------------
 
