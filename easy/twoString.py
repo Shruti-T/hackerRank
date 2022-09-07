0534e3542b241498ea28924dca240192293720ab
Shruti Tiwari <sh03shruti@gmail.com>
2022-09-07 22:06:03 +0530

    -

--- easy/twoString.py
+++ easy/twoString.py
@@ -2,14 +2,6 @@
 
 # ---------------------SOLUTION---------------------
 import os
-#
-# Complete the 'twoStrings' function below.
-#
-# The function is expected to return a STRING.
-# The function accepts following parameters:
-#  1. STRING s1
-#  2. STRING s2
-#
 
 def twoStrings(s1, s2):
     # Write your code here
@@ -27,8 +19,6 @@ def twoStrings(s1, s2):
             status=0
     if(status==0):
         return "NO"
-    
-    
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
